4b111f0c4d8cc648768c3b96d8853b2deee6efae
Bryce Thomas <bryce.m.thomas@gmail.com>
2022-04-03 20:42:10 -0700

    Add example drawing green triangle with OpenGL.
    This is based off the tutorial here:
    https://developer.android.com/training/graphics/opengl .  This commit gets as
    far as drawing a green triangle:
    https://developer.android.com/training/graphics/opengl .  I've tested this as
    working in the emulator.  This commit includes a bunch of extraneous boilerplate
    created by Android Studio, which I've included for the time being.

---
+++ README.md
@@ -0,0 +1,2 @@
+A repo for experimenting with graphics on Android.  So far has some basic OpenGL
+drawing code based on https://developer.android.com/training/graphics/opengl .
---
+++ app/src/main/java/com/example/androidgraphics/OpenGLES20Activity.kt
@@ -0,0 +1,18 @@
+package com.example.androidgraphics
+
+import android.app.Activity
+import android.opengl.GLSurfaceView
+import android.os.Bundle
+
+class OpenGLES20Activity : Activity() {
+    private lateinit var gLView: GLSurfaceView
+
+    public override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        // Create a GLSurfaceView instance and set it
+        // as the ContentView for this Activity.
+        gLView = MyGLSurfaceView(this)
+        setContentView(gLView)
+    }
+}
